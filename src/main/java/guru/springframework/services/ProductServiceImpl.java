69d0f1a43fb5fbb9b74a58e6784e233c8b662c43
Kiky <146445548+kiky97@users.noreply.github.com>
2026-08-01 00:00:23 +0200

    Update ProductServiceImpl.java

--- src/main/java/guru/springframework/services/ProductServiceImpl.java
+++ src/main/java/guru/springframework/services/ProductServiceImpl.java
@@ -4,11 +4,13 @@ import guru.springframework.domain.Product;
 import guru.springframework.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+///负责调用ProductRepository去操作product
 @Service
+
+///业务层的组件，请帮我创建它的对象
 public class ProductServiceImpl implements ProductService {
     private ProductRepository productRepository;
-
+///implements ProductService 实现了接口：ProductService
     @Autowired
     public void setProductRepository(ProductRepository productRepository) {
         this.productRepository = productRepository;
