44bf620e2ef879f13a8a0d80b41eb81c10c374f4
Kiky <146445548+kiky97@users.noreply.github.com>
2026-07-31 23:41:21 +0200

    Update ProductRepository.java

--- src/main/java/guru/springframework/repositories/ProductRepository.java
+++ src/main/java/guru/springframework/repositories/ProductRepository.java
@@ -3,5 +3,10 @@ package guru.springframework.repositories;
 import guru.springframework.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Integer>{
+public interface ProductRepository 
+  extends CrudRepository<Product, Integer>{
 }
+///数据访问
+操作product表格，id的类型是integer，
+  <实体类，主键类型>
+  
