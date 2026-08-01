85341b0aa8925b63d79225350ca2699ac803300b
Kiky <146445548+kiky97@users.noreply.github.com>
2026-08-01 18:02:41 +0200

    Update IndexController.java

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
+
+/负责接受浏览器发送过来的请求
 public class IndexController {
     @RequestMapping("/")
+    ///当有人访问“/”执行下面的方法， 把url和方法对应起来
     String index(){
         return "index";
+//index的页面
     }
 }
