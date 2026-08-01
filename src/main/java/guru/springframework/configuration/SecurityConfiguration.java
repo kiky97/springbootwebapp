cadd875da8261a6ad76252996c6fa8ce45ab3c3e
Kiky <146445548+kiky97@users.noreply.github.com>
2026-08-01 20:25:31 +0200

    Update SecurityConfiguration.java

--- src/main/java/guru/springframework/configuration/SecurityConfiguration.java
+++ src/main/java/guru/springframework/configuration/SecurityConfiguration.java
@@ -6,13 +6,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+///继承Spring Security的默认配置，然后修改它
+    
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
                 .authorizeRequests().antMatchers("/console/**").permitAll();
-
+///permitAll()允许所有的人访问，不用登录
+   ///     .and()继续配置 ,console里面的所有内容，任意层目录
         httpSecurity.csrf().disable();
+        ///
         httpSecurity.headers().frameOptions().disable();
     }
 
